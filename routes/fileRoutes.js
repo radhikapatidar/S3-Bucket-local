cfe30b972cc4a4ca7f3d1a60c314803824da9377
radhika patidar <radhikapatidar360@gmail.com>
2025-02-13 15:46:23 +0530

    change in route file

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -12,6 +12,6 @@ const upload = multer({ dest: 'uploads/' });
 router.post('/auth/token', generateToken); // Generate auth token
 // router.get('/upload-url', authenticate, getUploadUrl); // Get signed upload URL
 router.post('/upload', authenticate, upload.single('file'), uploadFile); // Upload file directly
-router.get('/file/:filename', authenticate, getFileUrl,streamFile); // Get signed download URL
+router.get('/file/:filename', authenticate, getFileUrl); // Get signed download URL
 
 module.exports = router;
